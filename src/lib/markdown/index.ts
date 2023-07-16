a271f142dfac68cb9113eb1a6fa33e197517f015
Zac Rosenbauer <zacrosenbauer@gmail.com>
2023-07-16 17:46:00 -0400

    expose items and remove need to test exports

--- src/lib/markdown/index.ts
+++ src/lib/markdown/index.ts
@@ -1,1 +1,3 @@
+export { supportedLanguages, SupportedLanguage, assertSupportedLanguage } from './codeBlock';
+export { supportedEmojis, EmojiAlias, EmojiUnicode, assertSupportedEmoji } from './emoji';
 export * as default from './markdown';
