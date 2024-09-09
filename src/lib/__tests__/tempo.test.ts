6ceea8bc4709878e69728fe351b8df93645c2712
Zac Rosenbauer <zacrosenbauer@gmail.com>
2024-09-09 14:33:26 -0400

    💽 incremental change

--- src/lib/__tests__/tempo.test.ts
+++ src/lib/__tests__/tempo.test.ts
@@ -1,10 +1,10 @@
 import { describe , it, expect } from 'vitest';
 
 import { TempoDoc } from '../tempo-doc';
-import tempo from '../tempo';
+import { createTempo } from '../tempo';
 
 describe('tempo', () => {
   it('should return a doc', () => {
-    expect(tempo()).toBeInstanceOf(TempoDoc);
+    expect(createTempo()).toBeInstanceOf(TempoDoc);
   });
 });
